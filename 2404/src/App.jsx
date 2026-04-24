e67a75a9efbf2b384c00888d6e913ccd1c72c1a4
williamcmarques8-ctrl <william.c.marques8@gmail.com>
2026-04-24 19:37:14 -0300

    commit

--- 2404/src/App.jsx
+++ 2404/src/App.jsx
@@ -1,12 +1,47 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [nomes,setNomes]= useState([
+    'William Campos',
+    'William Seila',
+    'William Marques',
+    'Willaim Nascimento'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  function adcionar() {
+    setNomes([...nomes, input]);
+    setInput("");
+  }
 
   return (
     <>
-      
+      <section id='center'>
+        
+        <div>
+          <ul>{ 
+            nomes.map( nome =>(
+              <li key={nome}>{nome}</li>
+            ))
+            }
+          </ul>
+        </div>
+        <div>
+          <p>
+            <input type='text' value = {input}
+            onChange={e => setInput(e.target.value)}
+            />
+            <button type = 'button' onClick={adcionar}>
+              Adcionar
+            </button>
+          </p>
+        </div>
+      </section>
     </>
   )
-}
+}; 
 
-export default App
+export default App;
